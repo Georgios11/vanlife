2e34bb9f46055ab79ae7d86ca4242fb307986ec2
Georgios11 <tsompanidis.georgios@gmail.com>
2024-12-17 12:59:59 +0100

    vans page done

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Vans = () => {
-	return <div>vans page</div>;
+	const [vans, setVans] = useState([]);
+	useEffect(() => {
+		const fetchVans = async () => {
+			try {
+				const response = await fetch("/api/vans");
+				const data = await response.json();
+
+				setVans(data.vans);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchVans();
+	}, []);
+	const vanElements = vans.map((van) => (
+		<div key={van.id} className="van-tile">
+			<img src={van.imageUrl} />
+			<div className="van-info">
+				<h3>{van.name}</h3>
+				<p>
+					${van.price}
+					<span>/day</span>
+				</p>
+			</div>
+			<i className={`van-type ${van.type} selected`}>{van.type}</i>
+		</div>
+	));
+	return (
+		<div className="van-list-container">
+			<h1>Explore our van options</h1>
+			<div className="van-list">{vanElements}</div>
+		</div>
+	);
 };
 
 export default Vans;
